de40772bb4dceaff06f0ce5bb7fe150463f8995f
Paulo Linhares <paulo.linhares@blu.com>
2021-02-06 16:19:25 +0100

    layout structure

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -9,16 +9,27 @@ export const Layout: React.FC = () => (
 );
 
 const LayoutContainer = styled.div`
-  display: flex;
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: 1fr auto;
+  grid-template-areas:
+    "main"
+    "footer";
+  height: 100vh;
+
+  background: rgba(0, 0, 0, 0.4);
 `;
 
 const Content = styled.main`
+  grid-area: main;
   background-color: tomato;
-  min-width: 500px;
-  min-height: 500px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 const Footer = styled.footer`
+  grid-area: footer;
   background-color: hotpink;
   width: 100%;
   height: 20px;
