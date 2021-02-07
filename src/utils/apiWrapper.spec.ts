b78c904ae0483fe0f614948e3492757946d2a74d
Paulo Linhares <paulo.linhares@blu.com>
2021-02-07 19:49:17 +0100

    fixes ts warnings due to mocking fetch on tests

--- src/utils/apiWrapper.spec.ts
+++ src/utils/apiWrapper.spec.ts
@@ -3,11 +3,12 @@ import eurTable from "../mock/baseEurConversion.json";
 
 describe("apiWrapper", () => {
   beforeEach(() => {
-    // tslint:disable-next-line
+    // @ts-ignore
     fetch.resetMocks();
   });
 
   it("should return eurTable when using mock", async () => {
+    // @ts-ignore
     fetch.mockResponseOnce(JSON.stringify(eurTable));
     const conversionTable = await loadConversionTable("EUR");
 
